f7aa3b91cdff8dfabe224c225bd5d305c083fabe
Anthony <splaces.project@gmail.com>
2021-11-29 01:44:48 +0300

    Documentation added

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Agent"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"BalancingApplication"},{"p":"<Unnamed>","l":"Stuff"},{"p":"<Unnamed>","l":"Workload"},{"p":"<Unnamed>","l":"WorkloadIntegrationTest"},{"p":"<Unnamed>","l":"WorkloadTest"}]
